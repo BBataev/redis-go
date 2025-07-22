bdf49fab9f825008db5e82e2731b5fca4a3835a1
dr0nched <beriven5o5@gmail.com>
2025-07-22 22:13:57 +0300

    [Feat] 3d task

--- app/main.go
+++ app/main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"net"
-	"os"
-)
-
-var (
-	_ = net.Listen
-	_ = os.Exit
+	"strings"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		panic(err)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		panic(err)
+	defer l.Close()
+	fmt.Println("Listening on :6379")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			continue
+		}
+		go handleConnection(conn)
 	}
-	conn.Write([]byte("+PONG\r\n"))
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewScanner(conn)
+	for reader.Scan() {
+		text := reader.Text()
+		if strings.TrimSpace(text) == "PING" {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+	}
 }
